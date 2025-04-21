c3a5fa3dadeed138dfc5876e48de084373bd120f
Anna Torres <anna.torres@escolar.ifrn.edu.br>
2025-04-21 09:45:27 -0300

    atividade-n1
    a atividade consiste em: "Desenvolva uma aplicação Flask, dentro de um ambiente virtual, que tenha uma rota onde o usuário especifica 2 números separados por / (barra). A função da rota deve somar esses números e retornar o resultado da soma para a página web."

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def bemvindo():
+    return 'olá, seja bem vindo! na barra de pesquisa digite os números que quer somar entre barras (/)'
+
+@app.route('/magic/<n1>/<n2>')
+def resultado(n1, n2):
+    soma = int(n1) + int(n2)
+    return f'a soma entre esses dois números é: {str(soma)}'
